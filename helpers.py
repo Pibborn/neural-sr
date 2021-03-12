38a7779199ac534f291ee378a3428e75feb9b145
Roberto Esposito <roberto.esposito@unito.it>
2021-03-12 16:09:36 +0100

    tau means and std logged to wandb

--- helpers.py
+++ helpers.py
@@ -14,12 +14,16 @@ def kendall_tau_per_query(y_pred, y, q):
         y_pred_q = tf.nn.softmax(y_pred[q == qi], axis=0)
         tau_list.append(kendalltau(y_q, y_pred_q)[0])
 
+    tau_mean = np.mean(tau_list)
+    tau_std = np.std(tau_list)
     data = [[s] for s in tau_list]
     table = wandb.Table(data=data, columns=["taus"])
     wandb.log({'tau_distribution_{}'.format(Globals.log_counts): wandb.plot.histogram(table, "taus", title=None)})
+    wandb.log({'tau_mean': tau_mean})
+    wandb.log({'tau_std': tau_std})
     Globals.log_counts += 1
 
-    return np.mean(tau_list), np.std(tau_list)
+    return np.mean(tau_mean), np.std(tau_std)
 
 
 class Constants:
