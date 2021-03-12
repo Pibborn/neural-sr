7027e1c079669f71ff8251a6956faa17b2fcab54
Mattia Cerrato <matcerrato@gmail.com>
2021-03-12 13:57:08 +0000

    Fixes histogram plot

--- helpers.py
+++ helpers.py
@@ -9,7 +9,7 @@ def kendall_tau_per_query(y_pred, y, q):
         y_q = y[q == qi]
         y_pred_q = tf.nn.softmax(y_pred[q == qi], axis=0)
         tau_list.append(kendalltau(y_q, y_pred_q))
-    plt.hist(tau_list, bins=np.arange(0., 1., 0.05), density=True)
+    plt.hist(tau_list, bins=np.arange(-1., 1., 0.1), histtype='step')
     plt.savefig('hist.png')
     return np.mean(tau_list), np.std(tau_list)
 
