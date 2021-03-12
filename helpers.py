2052555ee9b000686f0b73bc73b242832f4d5c3e
Roberto Esposito <roberto.esposito@unito.it>
2021-03-12 15:21:02 +0100

    tau histograms logged to wandb

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from scipy.stats import kendalltau
 import matplotlib.pyplot as plt
+import wandb
 
 def kendall_tau_per_query(y_pred, y, q):
     tau_list = []
@@ -9,8 +10,11 @@ def kendall_tau_per_query(y_pred, y, q):
         y_q = y[q == qi]
         y_pred_q = tf.nn.softmax(y_pred[q == qi], axis=0)
         tau_list.append(kendalltau(y_q, y_pred_q)[0])
-    plt.hist(tau_list, bins=np.arange(-1., 1., 0.1), histtype='step')
-    plt.savefig('hist.png')
+
+    data = [[s] for s in tau_list]
+    table = wandb.Table(data=data, columns=["taus"])
+    wandb.log({'tau_distribution': wandb.plot.histogram(table, "taus", title=None)})
+
     return np.mean(tau_list), np.std(tau_list)
 
 
